b8734416e12a5f1467df1be5859c42ab53cdbe1e
Oscar Ablinger <dev.ablinger@gmail.com>
2020-02-22 13:55:12 +0100

    feat(tokenlist): prettify

--- src/App/PlaygroundContainer/TestList/TokenList.tsx
+++ src/App/PlaygroundContainer/TestList/TokenList.tsx
@@ -1,19 +1,52 @@
 import React from 'react'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
+import { makeStyles } from '@material-ui/core/styles';
 import { Token } from 'moo'
 
+function isError(tokens: string|Token[]): boolean {
+    return typeof tokens === 'string'
+}
+
+function tokensToHTML(tokens: string|Token[]): string {
+    if (tokens instanceof Array) {
+        return tokens.map(token => JSON.stringify(token)).join('\n')
+    } else {
+        return tokens
+    }
+}
+
+const useStyles = makeStyles(theme => ({
+    tokenWrapper: {
+        overflowX: 'auto'
+    },
+    errorMessage: {
+        color: 'red',
+    },
+}))
+
 export function TokenList({ tokens }: { tokens: string|Token[]}) {
+    const classes = useStyles()
+
+    return <div
+        aria-label="list of resulting tokens or the error message"
+        className={`${classes.tokenWrapper} ${isError(tokens) ? classes.errorMessage : ''}`}
+    >
+        <pre>
+            <code dangerouslySetInnerHTML={{__html: tokensToHTML(tokens)}}></code>
+        </pre>
+    </div>
+
+    /*
     function interpretAsAListOfTokens(tokens: Token[]) {
         return tokens.map((token, idx) => <ListItem dense key={idx}>
-            <ListItemText primary={JSON.stringify(token)} />
+            <ListItemText primary={<pre><code dangerouslySetInnerHTML={{__html: JSON.stringify(token)}}></code></pre>} />
             </ListItem>)
     }
 
     function interpretAsAnError(errorMsg: string) {
         return <ListItem>
-            <ListItemText primary={errorMsg} primaryTypographyProps={{color: 'error'}} />
+            <pre>
+                <code dangerouslySetInnerHTML={{__html: errorMsg}} style={{color: 'red'}}></code>
+            </pre>
         </ListItem>
     }
 
@@ -24,4 +57,5 @@ export function TokenList({ tokens }: { tokens: string|Token[]}) {
     return <List component="nav" aria-label="list of resulting tokens (or the error message)">
         {inner}
       </List>
+      */
 }
